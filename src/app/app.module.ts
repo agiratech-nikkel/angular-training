81e253ee95679aad684e5d9042124beab2a15876
nikkel <nikkel.r@agiratech.com>
2022-10-11 12:06:36 +0530

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,16 @@ import { LoginpageComponent } from './loginpage/loginpage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared-module/shared/shared.module';
-import { TestComponent } from './test/test.component';
 import { Test1Component } from './test1/test1.component';
 import { Test2Component } from './test2/test2.component';
-import { Testing1Component } from './testing1/testing1.component';
-import { Testing2Component } from './testing2/testing2.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginpageComponent,
-    TestComponent,
     Test1Component,
     Test2Component,
-    Testing1Component,
-    Testing2Component,
   ],
   imports: [
     BrowserModule,
